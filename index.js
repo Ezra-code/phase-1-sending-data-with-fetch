aa624056a234dc8aef51c7da651f7448bae0eb42
Ezra-code <njerukithinji872@gmail.com>
2022-06-14 19:57:42 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData(name, email){
+    let formData = {
+        name,
+        email
+    }
+   return fetch("http://localhost:3000/users",{
+        method:"POST",
+        headers:{
+            "content-type":"application/json",
+            accept:"application/json"
+        },
+        body:JSON.stringify(formData)
+    })
+    .then(res => res.json())
+    .then(jsonData =>{
+        document.body.innerHTML = jsonData.id
+    })
+    .catch(message => document.body.innerHTML = message)
+}
